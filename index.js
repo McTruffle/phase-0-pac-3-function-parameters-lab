2bd56e33f354f0508cdbdb2967ec1c49e76d7b08
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-05-25 03:48:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function introductionWithLanguageOptional(name, language = "Javascript") {
-    console.log(`Hello, my name is ${name} and I am learning to program with ${language}!`);
-  }
+function introduction(name) {
+  return (`Hi, my name is ${name}.`);
+};
+
+function introductionWithLanguage(name, language) {
+  return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+};
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  };
+
